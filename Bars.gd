023772079e7880e7423313901dbdf5feb9b28078
Țeculescu Octavian-Alexandru <octav_teculescu@yahoo.com>
2020-09-11 19:55:55 +0300

    Fixed death conditions and added states

--- Bars.gd
+++ Bars.gd
@@ -8,15 +8,27 @@ func _ready():
 
 func _on_Player_courage_changed(courage):
 	$Courage.value = courage
+	if courage <= 0:
+		get_parent().get_node("State").modulate = Color(1, 0, 0)
+		get_parent().get_node("State").visible = true
 
 
 func _on_Player_hope_changed(hope):
 	$Hope.value = hope
+	if hope <= 0:
+		get_parent().get_node("State").modulate = Color(0, 1, 1)
+		get_parent().get_node("State").visible = true
 
 
 func _on_Player_sanity_changed(sanity):
 	$Sanity.value = sanity
+	if sanity <= 0:
+		get_parent().get_node("State").modulate = Color(1, 1, 0)
+		get_parent().get_node("State").visible = true
 
 
 func _on_Player_serenity_changed(serenity):
 	$Serenity.value = serenity
+	if serenity <= 0:
+		get_parent().get_node("State").modulate = Color(0, 1, 0)
+		get_parent().get_node("State").visible = true
--- Player.gd
+++ Player.gd
@@ -15,23 +15,45 @@ var hope : int = 100
 
 var player_turn : bool
 
+var state : String
 
 func take_damage(damage : int, damage_type : String) -> void:
 	match damage_type.to_lower():
 		"sanity":
 			sanity -= damage
+			if sanity <= 0:
+				if state.empty():
+					state = "insane"
+				else:
+					emit_signal("player_died")
 			emit_signal("sanity_changed", sanity)
+			
 		"courage":
 			courage -= damage
+			if courage <= 0:
+				if state.empty():
+					state = "terrified"
+				else:
+					emit_signal("player_died")
 			emit_signal("courage_changed", courage)
+			
 		"serenity":
 			serenity -= damage
+			if serenity <= 0:
+				if state.empty():
+					state = "enraged"
+				else:
+					emit_signal("player_died")
 			emit_signal("serenity_changed", serenity)
+			
 		"hope":
 			hope -= damage
+			if sanity <= 0:
+				if state.empty():
+					state = "hysteric"
+				else:
+					emit_signal("player_died")
 			emit_signal("hope_changed", hope)
-	if sanity <= 0 or courage <= 0 or serenity <= 0 or hope <= 0:
-		emit_signal("player_died")
 
 
 func _on_Prototype_player_turn():
